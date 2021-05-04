7233332ed0f26d241609399563e4090e705314d5
L3af <L3afMeAlon3@gmail.com>
2021-05-04 04:55:41 +0000

    Actually update overlay

--- config/nixpkgs/overlays/neovim-nightly.nix
+++ config/nixpkgs/overlays/neovim-nightly.nix
@@ -1,3 +1,3 @@
 import (builtins.fetchTarball {
-  url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+  url = https://github.com/nix-community/neovim-nightly-overlay/archive/dad48e1edca040a68c51c1433606b57cfdf027dc.tar.gz;
 })
